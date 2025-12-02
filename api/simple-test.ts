ba2a843e4a3afef21a14c0e1f0b684c6e020c20a
insightout11 <insightout11@gmail.com>
2025-12-02 13:58:43 +0700

    Add simple test function

---
+++ api/simple-test.ts
@@ -0,0 +1,17 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return res.json({
+    message: 'Simple test works',
+    timestamp: new Date().toISOString(),
+    env: process.env.NODE_ENV
+  });
+}
